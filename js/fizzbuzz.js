d2333283a152efabd8748cd222a3aa8e5051d9ee
Zakaria96 <zakarialavoro@gmail.com>
2023-05-02 17:08:24 +0200

    esercizio completato

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -11,16 +11,18 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”*/
 ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.*/
 for (let i = 1 ; i <= 100 ; i++){
     if (i % 3 === 0 && i % 5 === 0) {// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
-        risultato = "FizzBuzz"
+        risultato = "FizzBuzz" 
+        container.innerHTML += `<div class="square fizzbuzz">${risultato}</div>`;
     } else if(i % 5 === 0) {  // per i multipli di 5 stampi “Buzz”
-        risultato = "Buzz" 
+        risultato = "Buzz"
+        container.innerHTML += `<div class="square buzz">${risultato}</div>`; 
     } else if (i % 3 === 0) { //per i multipli di 3 stampi “Fizz”
         risultato = "Fizz"
+        container.innerHTML += `<div class="square fizz">${risultato}</div>`;
     } else {
-        risultato = i + " "; 
+        risultato = i + " ";
+        container.innerHTML += `<div class="square">${risultato}</div>`;
     }
-   const number =`<div class="square">${risultato}</div>`;
-   container.innerHTML += number
 }
 
 
