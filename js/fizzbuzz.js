a62de7bdb85709f78dc4535cc96464e75a7d15c7
Zakaria96 <zakarialavoro@gmail.com>
2023-05-02 15:51:49 +0200

    programma da 1 a 100 fatto

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -3,17 +3,21 @@ ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipl
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”*/
 
 // variabili globali 
-
+ let risultato;
 // Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 for (let i = 1; i <= 100 ; i++){
-    let risultato = i ;
+    if (i % 3 === 0) { //per i multipli di 3 stampi “Fizz”
+        risultato += "Fizz"
+    } else if(i % 5 === 0) {  // per i multipli di 5 stampi “Buzz”
+        risultato += "Buzz"
+    } else {
+        risultato += i + " "; 
+    }
     console.log(risultato);
 }
 
-// ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 
-// per i multipli di 3 stampi “Fizz”
 
-// per i multipli di 5 stampi “Buzz”
 
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
