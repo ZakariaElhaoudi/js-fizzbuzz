2e6da1d95f4d2ce751f48f2c933304c091cf9424
Zakaria96 <zakarialavoro@gmail.com>
2023-05-02 16:50:58 +0200

    correzione errore fizzbuzz

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -10,7 +10,7 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”*/
 /*Scrivi un programma che stampi in console i numeri da 1 a 100, 
 ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.*/
 for (let i = 1 ; i <= 100 ; i++){
-    if (i % 3 === 0 && i % 3 === 0) {// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
+    if (i % 3 === 0 && i % 5 === 0) {// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
         risultato = "FizzBuzz"
     } else if(i % 5 === 0) {  // per i multipli di 5 stampi “Buzz”
         risultato = "Buzz" 
